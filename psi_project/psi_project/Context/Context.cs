8b3765dab690b61864e064a57e4de6b088ec0063
LAIZ FERNANDA SOUZA DOS SANTOS <laiz.santos@academico.ifrn.edu.br>
2022-05-30 10:24:57 -0300

    novo projeto

---
+++ psi_project/psi_project/Context/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace psi_project.Context
+{
+    public class Context
+    {
+        public long FabricanteId { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ psi_project/psi_project/Controllers/FabricantesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace psi_project.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.Ordeby(c => c.Nome);
+        }
+    }
+}
