6b8236c5c7eed4cf22ebf60150fe34bca0fe0976
LAIZ FERNANDA SOUZA DOS SANTOS <laiz.santos@academico.ifrn.edu.br>
2022-10-31 10:24:39 -0300

    Erro na linha 17-PRODUTODAL

--- WebAppProjeto01G1/Persistenciaa/Contexts/EFContext.cs
+++ WebAppProjeto01G1/Persistenciaa/Contexts/EFContext.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Web;
 using Modelo;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using Persistenciaa.Migrations;
 
 namespace Persistenciaa.Contexts
 {
     public class EFContext : DbContext
     {
         public EFContext() : base("Asp_Net_MVC_CS"){
-            Database.SetInitializer<EFContext>(
-            new DropCreateDatabaseIfModelChanges<EFContext>());
+            //Database.SetInitializer<EFContext>(
+            // new DropCreateDatabaseIfModelChanges<EFContext>());
+            Database.SetInitializer<EFContext>(new
+            MigrateDatabaseToLatestVersion<EFContext, Configuration>());
         }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
---
+++ WebAppProjeto01G1/Persistenciaa/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace Persistenciaa.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Persistenciaa.Contexts.EFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "Persistenciaa.Contexts.EFContext";
+        }
+
+        protected override void Seed(Persistenciaa.Contexts.EFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
